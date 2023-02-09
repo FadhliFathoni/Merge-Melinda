8a500230a10de42960fd8251c675bcd43f284c20
FadhliFathoni <fadhlifathoni62@gmail.com>
2023-02-09 20:42:18 +0700

    Add data and fix pagination

--- API/ManagementUser/views.py
+++ API/ManagementUser/views.py
@@ -9,7 +9,12 @@ from rest_framework.response import Response
 from Account.models import Account
 
 class UserPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 5
+    def get_page_size(self, request):
+        if "limit" in request.GET:
+            self.page_size = request.GET["limit"]
+        return super().get_page_size(request)
+
 
 class ListUser(ListAPIView):
     queryset = Account.objects.all()
@@ -17,6 +22,8 @@ class ListUser(ListAPIView):
     filter_backends = [OrderingFilter, SearchFilter]
     search_fields = ("username","email","date__joined")
     pagination_class = UserPagination
+
+        
 
 @api_view(["DELETE"])
 def deleteUser(request,delete_id):
