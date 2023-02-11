501b25a1e4cd385c7167d8b589d199020a1af99e
Diaz-adrianz <diazadrianz191205@gmail.com>
2023-02-11 16:13:42 +0700

    poho date time field

--- API/Produk/models.py
+++ API/Produk/models.py
@@ -43,8 +43,8 @@ class Produk(models.Model):
         null=True
     )
     
-    created = models.DateField(auto_now_add=True)
-    updated = models.DateField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nama
