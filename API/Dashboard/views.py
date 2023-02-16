aab5399853f5117c21e32112f6b6870b267725ae
FadhliFathoni <fadhlifathoni62@gmail.com>
2023-02-16 23:21:48 +0700

    change created into updated

--- API/Dashboard/views.py
+++ API/Dashboard/views.py
@@ -8,22 +8,22 @@ from dateutil.relativedelta import relativedelta
 @api_view(["GET"])
 def dashboard(request):
     now = datetime.now().date()
-    minyak = Minyak.objects.filter(status = "Terverifikasi",created__icontains = now)
+    minyak = Minyak.objects.filter(status = "Terverifikasi",updated__icontains = now)
     user = User.objects.filter(createdAt__icontains = now)
-    setor = Minyak.objects.filter(status = "Terverifikasi", created__icontains = now)
+    setor = Minyak.objects.filter(status = "Terverifikasi", updated__icontains = now)
     totalMinyak = 0
     if "date" in request.GET:
         if request.GET["date"] == "week":
-            minyak = Minyak.objects.filter(status="Terverifikasi",created__range = [now-relativedelta(weeks=1),now])
-            setor = Minyak.objects.filter(status = "Terverifikasi", created__range = [now-relativedelta(weeks=1),now])
+            minyak = Minyak.objects.filter(status="Terverifikasi",updated__range = [now-relativedelta(weeks=1),now])
+            setor = Minyak.objects.filter(status = "Terverifikasi", updated__range = [now-relativedelta(weeks=1),now])
             user = User.objects.filter(createdAt__range = [now-relativedelta(weeks=1),now])
         elif request.GET["date"] == "month":
-            minyak = Minyak.objects.filter(status="Terverifikasi",created__range = [now-relativedelta(months=1),now])
-            setor = Minyak.objects.filter(status = "Terverifikasi", created__range = [now-relativedelta(months=1),now])
+            minyak = Minyak.objects.filter(status="Terverifikasi",updated__range = [now-relativedelta(months=1),now])
+            setor = Minyak.objects.filter(status = "Terverifikasi", updated__range = [now-relativedelta(months=1),now])
             user = User.objects.filter(createdAt__range = [now-relativedelta(months=1),now])
         elif request.GET["date"] == "year":
-            minyak = Minyak.objects.filter(status="Terverifikasi",created__range = [now-relativedelta(years=1),now])
-            setor = Minyak.objects.filter(status = "Terverifikasi", created__range = [now-relativedelta(years=1),now])
+            minyak = Minyak.objects.filter(status="Terverifikasi",updated__range = [now-relativedelta(years=1),now])
+            setor = Minyak.objects.filter(status = "Terverifikasi", updated__range = [now-relativedelta(years=1),now])
             user = User.objects.filter(createdAt__range = [now-relativedelta(years=1),now])
     for x in minyak:
         totalMinyak = totalMinyak + x.volume
